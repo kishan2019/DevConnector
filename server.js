5845e74fe3be815f147b5787208f5c703a587959
kishan2019 <kishandesc@gmail.com>
2020-02-20 01:06:22 +0530

    added routes

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+//connect database
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('helllo');
-})
+//Define Routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const port = process.env.PORT || 5000;
 
