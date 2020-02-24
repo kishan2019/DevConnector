a7b3e87c7a781148dfedb88344fc0595ebd6c934
kishan2019 <kishandesc@gmail.com>
2020-02-24 18:17:32 +0530

    Added route to the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import { Navbar } from './components/layouts/Navbar';
 import { Landing } from './components/layouts/Landing';
+import { Login } from './components/auth/Login'
+import { Register } from './components/auth/Register';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <div className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </div>
+    </Fragment>
+  </Router>
+
+
 );
 
 
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,21 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Landing = () => {
-    return (
-        <section className="landing">
+  return (
+    <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Welcome to Twitter</h1>
           <p className="lead">
-          Twitter lets you follow your interests. Follow people that you want to hear from.
-           Your timeline is a custom stream of your follows.
+            Twitter lets you follow your interests. Follow people that you want to hear from. Your timeline is a custom stream of your follows.
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
     </section>
-    )
+  )
 }
