f91877e57ce1c1675ae2497c06049428b344613c
kishan2019 <kishandesc@gmail.com>
2020-03-07 15:56:23 +0530

    added logout.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Navbar } from './components/layouts/Navbar';
+import Navbar from './components/layouts/Navbar';
 import { Landing } from './components/layouts/Landing';
 import Login from './components/auth/Login'
 import Register from './components/auth/Register';
